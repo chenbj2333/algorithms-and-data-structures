ef610306f08fa17558f0468cb05466d2ec53e9ad
chenbingjie <chenbingjie@ghostcloud.cn>
2021-04-21 20:03:47 +0800

    栈的实现

---
+++ src/Stack.ts
@@ -0,0 +1,37 @@
+interface IStack<T> {
+  push: (value: T) => void;
+  pop: () => T | undefined;
+  peek: () => T | undefined;
+  getSize: () => number;
+  isEmpty: () => boolean;
+}
+
+class Stack<T> implements IStack<T> {
+  private array: T[];
+
+  constructor(arr: T[]) {
+    this.array = arr;
+  }
+
+  public push(item: T): void {
+    this.array.push(item);
+  }
+
+  // 删除栈底元素并返回该元素
+  public pop() {
+    return this.array.pop();
+  }
+
+  // 删除栈顶元素并返回该元素
+  public peek() {
+    return this.array.shift();
+  }
+
+  public getSize() {
+    return this.array.length;
+  }
+
+  public isEmpty() {
+    return this.array.length === 0;
+  }
+}
